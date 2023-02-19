40184dabd1c3db160c8a4ee85991ce403d031d9f
unknown <dcarol.prince@gmail.com>
2023-02-18 20:44:15 -0600

    final Js

--- main.js
+++ main.js
@@ -5,4 +5,13 @@ if(!audio) return;
 audio.currentTime = 0;
 audio.play();
 console.log(key);
+key.classList.add('playing');
 });
+
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+ this.classList.remove('playing');
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
